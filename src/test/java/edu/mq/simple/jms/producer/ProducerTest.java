21fc6f4019f4d61693599d2b0e454bbb02090834
Artem Pronchakov <artem.pronchakov@gmail.com>
2023-09-27 01:03:59 +0300

    fix producer test

--- src/test/java/edu/mq/simple/jms/producer/ProducerTest.java
+++ src/test/java/edu/mq/simple/jms/producer/ProducerTest.java
@@ -3,12 +3,9 @@ package edu.mq.simple.jms.producer;
 import edu.mq.simple.jms.connection.SimpleMQConnectionFactory;
 import edu.mq.simple.test.TestUtils;
 import jakarta.jms.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
-public class MessageProducerTest {
+public class ProducerTest {
 
     private SimpleMQConnectionFactory connectionFactory;
     private Connection connection;
@@ -17,7 +14,6 @@ public class MessageProducerTest {
     private MessageProducer producer;
 
     @BeforeEach
-    @AfterEach
     public void before() throws JMSException {
         TestUtils.deleteQueue("./db", "edu.queue.q1");
         connectionFactory = SimpleMQConnectionFactory.builder()
@@ -29,7 +25,7 @@ public class MessageProducerTest {
         producer = session.createProducer(queue);
     }
 
-    @BeforeEach
+    @AfterEach
     public void after() throws JMSException {
         producer.close();
         session.close();
