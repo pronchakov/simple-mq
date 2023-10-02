b948935372c184f8cb8fbeb6f8fa11a43b78c233
Artem Pronchakov <artem.pronchakov@gmail.com>
2023-10-03 02:07:25 +0300

    add multiple consumers and connections test. connections are now duplicating readed messages, but for now it's fine

--- src/test/java/edu/mq/simple/jms/consumer/MultipleConsumersTest.java
+++ src/test/java/edu/mq/simple/jms/consumer/MultipleConsumersTest.java
@@ -3,36 +3,41 @@ package edu.mq.simple.jms.consumer;
 import edu.mq.simple.jms.connection.SimpleMQConnectionFactory;
 import edu.mq.simple.test.TestUtils;
 import jakarta.jms.Message;
+import jakarta.jms.Session;
 import jakarta.jms.TextMessage;
 import lombok.AllArgsConstructor;
 import lombok.Cleanup;
 import lombok.SneakyThrows;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class MultipleConnectionsTest {
+public class MultipleConsumersTest {
 
     @SneakyThrows
     @Test
-    public void testMultipleConnections() {
-        TestUtils.deleteQueue("./db", "edu.queue.multiple-connections");
+    public void testMultipleConsumers() {
+        TestUtils.deleteQueue("./db", "edu.queue.multiple-consumers");
 
         final var connectionFactory = SimpleMQConnectionFactory.builder()
                 .baseDir("./db")
                 .build();
         @Cleanup final var connection = connectionFactory.createConnection();
         @Cleanup final var session = connection.createSession();
-        final var queue = session.createQueue("edu.queue.multiple-connections");
+        final var queue = session.createQueue("edu.queue.multiple-consumers");
         @Cleanup final var producer = session.createProducer(queue);
 
-        final var messageCount = 10_000;
+        final var messageCount = 20_000;
         for (int i = 0; i < messageCount; i++) {
             producer.send(session.createTextMessage("hello-" + String.format("%05d", i)));
         }
@@ -41,42 +46,48 @@ public class MultipleConnectionsTest {
 
         final var futures = new CompletableFuture[availableProcessors];
         for (int i = 0; i < availableProcessors; i++) {
-            final var future = CompletableFuture.supplyAsync(new MessagesSupplier(connectionFactory));
+            final var future = CompletableFuture.supplyAsync(new MultipleConsumersTest.MessagesSupplier(session));
             futures[i] = future;
         }
         CompletableFuture.allOf(futures).get();
 
-        for (CompletableFuture future : futures) {
-            final var stringList = (List<String>) future.get();
-            Assertions.assertEquals(messageCount, stringList.size());
-            Collections.sort(stringList);
-            for (int i = 0; i < messageCount; i++) {
-                Assertions.assertEquals("hello-" + String.format("%05d", i), stringList.get(i));
-            }
+        final var messageTexts = Arrays.stream(futures)
+                .flatMap(MultipleConsumersTest::apply)
+                .collect(Collectors.toList());
+
+        Assertions.assertEquals(messageCount, messageTexts.size());
+
+        Collections.sort(messageTexts);
+
+        for (int i = 0; i < messageCount; i++) {
+            Assertions.assertEquals("hello-" + String.format("%05d", i), messageTexts.get(i));
         }
 
-        TestUtils.deleteQueue("./db", "edu.queue.multiple-connections");
+        TestUtils.deleteQueue("./db", "edu.queue.multiple-consumers");
     }
 
     @AllArgsConstructor
     private static class MessagesSupplier implements Supplier<List<String>> {
 
-        private SimpleMQConnectionFactory connectionFactory;
+        private Session session;
 
         @SneakyThrows
         @Override
         public List<String> get() {
-            @Cleanup final var connection = connectionFactory.createConnection();
-            @Cleanup final var session = connection.createSession();
-            final var queue = session.createQueue("edu.queue.multiple-connections");
+            final var queue = session.createQueue("edu.queue.multiple-consumers");
             @Cleanup final var consumer = session.createConsumer(queue);
 
             final var result = new ArrayList<String>(1000);
 
+//            int i = 0;
             Message message;
             while ((message = consumer.receiveNoWait()) != null) {
                 final var messageText = ((TextMessage) message).getText();
                 result.add(messageText);
+//                i++;
+//                if (i % 1000 == 0) {
+//                    System.out.println(Thread.currentThread().getName() + " read " + i);
+//                }
             }
 
             return result;
